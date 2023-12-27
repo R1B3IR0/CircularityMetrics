213115fdccd85c519b912ba7359e3d88274cf099
paulo <8190465@estg.ipp.pt>
2023-12-27 15:03:07 +0000

    codigo e testes feitos e corrigidos

--- src/main/java/productsystem/AggregatedProcess.java
+++ src/main/java/productsystem/AggregatedProcess.java
@@ -3,27 +3,27 @@ package productsystem;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-
+//subclasse de Process
 public class AggregatedProcess extends Process{
-    private List<UnitaryProcess> container;
-    private int count;
+    private List<UnitaryProcess> container;//lista para armazenar Processos Unitários
+    private int count;//contador de processos
     public AggregatedProcess(String name) {
         super(name);
         this.container=new ArrayList<>();
         this.count=0;
     }
-
+//método para encontrar a posição de um processo pelo seu id
     public int findUnitaryProcess(int id){
-        int position=0;
-        for(UnitaryProcess un:this.container){
-            if(un.getId()==id){
-                return position;
+        int position = 0;
+        for (UnitaryProcess un : this.container) {
+            if (un.getId() == id) {
+                return position; // devolve a posição correta se encontrado
             }
             position++;
         }
-        return -1;
+        return -1; // caso nao encontre retorna -1
     }
-
+//método para adicionar um Unitary Process
     public UnitaryProcess addUnitaryProcess(UnitaryProcess un){
         if(un == null){
             throw new NullPointerException("Unitary Process cannot be null");
@@ -32,16 +32,16 @@ public class AggregatedProcess extends Process{
         count++;
         return un;
     }
-
+//método para remover um processo
     public UnitaryProcess removeUnitaryProcess(int id){
-        int pos=findUnitaryProcess(id);
+        int pos=findUnitaryProcess(id);//encontra a posicao
         if(pos==-1){
             throw new NoSuchElementException(id + " doesn't exist");
         }
         UnitaryProcess un = this.container.get(pos);
         this.container.remove(pos);
         count--;
-        return un;
+        return un;//devolve o processo removido
     }
 
     public List<UnitaryProcess> getContainer() {
