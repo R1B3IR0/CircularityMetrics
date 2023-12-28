1d9fcdfe54370eeb352e9c0a0d77909f88f9f777
pedroteixeira2002 <8200489@estg.ipp.pt>
2023-12-28 11:31:20 +0000

    Código refrente ao ProductList

--- src/main/java/productsystem/ProductList.java
+++ src/main/java/productsystem/ProductList.java
@@ -1,34 +1,60 @@
 package productsystem;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProductList implements CircularityInfo {
+public class ProductList {
     private List<Product> list;
     private String name;
     private int count;
 
     public ProductList(String name) {
+        this.name = name;
+        this.count = 0;
+        this.list = new ArrayList<>();
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
+
     public void setName(String name) {
+        this.name = name;
     }
+
     public int getCount() {
-        return 0;
+        return this.count;
     }
-    public Product addProduct(Product product){
-        return null;
+
+    public Product addProduct(Product product) {
+        if (this.list.add(product) == true) {
+            this.count++;
+            return product;
+        } else return null;
     }
-    public Product removeProduct(String name){
-        return null;
+
+    public Product removeProduct(String name) {
+        int index = findProduct(name);
+        if(index!=-1){
+            this.count--;
+            return this.list.remove(index);
+        }
+        else return null;
     }
-    public int findProduct(String name){
-        return 0;
+
+    public int findProduct(String name) {
+        for (Product product : this.list) {
+            if (product.getName().equals(name))
+                return this.list.indexOf(product);
+        }
+        return -1;
     }
+
     @Override
-    public List<ProductList> sendList() {
-        return null;
+    public String toString() {
+        return "ProductList{" +
+                "list: " + list.toString()+
+                "Name: " + name ;
     }
 }
+
