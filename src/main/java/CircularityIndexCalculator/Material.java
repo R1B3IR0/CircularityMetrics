ea4993b39094df65ae91aca759872fba18e364e6
paulo <8190465@estg.ipp.pt>
2024-01-05 12:05:23 +0000

    esqueleto da class para testes

---
+++ src/main/java/CircularityIndexCalculator/Material.java
@@ -0,0 +1,4 @@
+package CircularityIndexCalculator;
+
+public class Material {
+}
---
+++ src/main/java/CircularityIndexCalculator/MaterialList.java
@@ -0,0 +1,50 @@
+package CircularityIndexCalculator;
+
+import java.util.List;
+import productsystem.*;
+import productsystem.Process;
+
+public class MaterialList {
+    List<Material> list;
+    Process process;
+
+    public MaterialList() {
+    }
+
+    private List<String> getMaterialName(Process p ){
+        return null;
+    }
+    private double getVirginMaterial(String name,Process p){
+        return 0;
+    }
+    private double getRecoveredMaterial(String name,Process p){
+        return 0;
+    }
+    private double getRecycledWaste(String name,Process p){
+        return 0;
+    }
+    private double getRadioctiveWaste(String name,Process p){
+        return 0;
+    }
+    private double getRecycledMaterial(String name,Process p){
+        return 0;
+    }
+    private double getMainEnergy(String name,Process p){
+        return 0;
+    }
+    private double getSecondaryEnergy(String name,Process p){
+        return 0;
+    }
+    private double getWasteProduction(String name,Process p){
+        return 0;
+    }
+    private Material createMaterial(String name,Process p){
+        return null;
+    }
+    private void createAllMaterial(String name,Process p){
+    }
+    public List<Material> getMaterials(){
+        return null;
+    }
+
+}
