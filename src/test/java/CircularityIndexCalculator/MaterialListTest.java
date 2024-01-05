bc54965204b72a7fe1a0081c0ab1ae889e981b87
paulo <8190465@estg.ipp.pt>
2024-01-05 12:07:18 +0000

    esqueleto da class para testes

---
+++ src/test/java/CircularityIndexCalculator/MaterialListTest.java
@@ -0,0 +1,12 @@
+package CircularityIndexCalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaterialListTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
