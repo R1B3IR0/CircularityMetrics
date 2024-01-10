118a326bf616f4cfba706957137811262817ac1e
paulo <8190465@estg.ipp.pt>
2024-01-10 21:26:55 +0000

    Criação dos testes black-box de ImportCsv

---
+++ src/main/java/dataPersistance/ImportCsv.java
@@ -0,0 +1,36 @@
+package dataPersistance;
+
+import productsystem.Product;
+
+import java.util.List;
+
+public class ImportCsv {
+    private String filePath;
+    private List<Row> list;
+
+    public ImportCsv(){
+
+    }
+
+    public List<Row> importFlowsFromCSV(String filePath) {
+        return null;
+    }
+    public List<String> getProcessNames() {
+        return null;
+    }
+
+    public Product createProcess(List<String> lista) {
+        return null;
+    }
+    public void createUnitaryProcess(Product product) {
+        return;
+    }
+    public void createAggregatedProcess(Product product){
+        return;
+    }
+
+    public Product sendProduct(){
+        return null;
+    }
+}
+
